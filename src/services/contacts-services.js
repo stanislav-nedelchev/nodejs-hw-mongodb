00dbfd593e84bb8c1d96634c8708457b66c6ef85
stanislav_nedelchev <stasnedel@gmail.com>
2024-12-24 21:09:09 +0200

    finish hw-02

---
+++ src/services/contacts-services.js
@@ -0,0 +1,4 @@
+import ContactCollection from '../db/models/Contacts.js';
+
+export const getContacts = () => ContactCollection.find();
+export const getContactById = (id) => ContactCollection.findById(id);
